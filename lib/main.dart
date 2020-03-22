bcda7e8c3619087dc713b76f5e274a82adccfe1f
Devair Luiz Moitim <dmoitim@gmail.com>
2020-03-22 10:33:43 -0300

    Exibe a lista de items e seta o estado se marcado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -16,18 +18,43 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Todo List"),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Olá mundo!"),
+        appBar: AppBar(
+          title: Text("Todo List"),
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = widget.items[index];
+
+            return CheckboxListTile(
+              title: Text(item.title),
+              key: Key(UniqueKey().toString()),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            );
+          },
+        ));
   }
 }
